68d53f00294099ea4590251d92a6889bf0e8e9a4
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-11 11:40:02 +0000

    Pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+
+def pascal_triangle(n):
+    Tlist = []
+    for i in range(n):
+        temp_list = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(Tlist[i-1][j] + Tlist[i-1][j-1])
+        Tlist.append(temp_list)
+    return Tlist
