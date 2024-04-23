a8c7ede66533fa70d19b4b038cbc41d1645c1ff8
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-23 12:21:51 +0100

    fix style

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,6 @@
 def minOperations(n):
     """Minimum Operations"""
     if n <= 1:
-        # If n is less than or equal to 1, it's not possible to perform operations.
         return 0
     
     operations = 0
@@ -13,7 +12,6 @@ def minOperations(n):
     
     while factor <= n:
         while n % factor == 0:
-            # Increment the operations by the factor count
             operations += factor
             n //= factor
         factor += 1
