abc906ba30b54e42b81f97a4c808039421289212
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-07 02:56:17 +0100

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,18 +3,19 @@
 
 
 def makeChange(coins, total):
-    """makechange"""
+    """Determine the fewest number of coins needed"""
     if total <= 0:
         return 0
 
-    # Initialize dp array with a large value (total + 1)
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    # Update dp array
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    return dp[total] if dp[total] != total + 1 else -1
+    if dp[total] != total + 1:
+        return dp[total]
+    else:
+        return -1
 
